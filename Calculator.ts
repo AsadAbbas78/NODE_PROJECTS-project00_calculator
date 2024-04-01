f23d999d4973eb5cc19518a7a8c764badc5f594b
AsadAbbas78 <assadgamerchannel1@gmail.com>
2024-04-01 15:21:50 +0500

    first commit

---
+++ Calculator.ts
@@ -0,0 +1,47 @@
+import inquirer from "inquirer";
+
+const answer: {
+  numberOne: number;
+  numberTwo: number;
+  operator: string;
+} = await inquirer.prompt([
+  {
+    type: "number",
+    name: "number one",
+    message: "kindly enter your first number",
+  },
+
+  {
+    type: "number",
+    name: "number Two",
+    message: "kindly enter your second number",
+  },
+
+  {
+    type: "list",
+    name: "Operator",
+    choices: ["*", "+", "-", "/"],
+    message: "select Operator: ",
+  },
+]);
+
+const { numberOne, numberTwo, operator } = answer;
+if (numberOne && numberTwo && operator) {
+  let result: number = 0;
+  if (operator === "+") {
+    result = numberOne + numberTwo;
+  } else if (operator === "-") {
+    result = numberOne - numberTwo;
+  }
+
+  if (operator === "/") {
+    result = numberOne / numberTwo;
+  }
+  if (operator === "*") {
+    result = numberOne * numberTwo;
+  }
+
+  console.log("your result is :", result);
+} else {
+  console.log("kindly enter a valid input");
+}
